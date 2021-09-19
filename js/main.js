4b8f1a7da98656c29fd676166ae2d0917aa3763a
DaisukeYou <daisukeukawa007@gmail.com>
2021-09-19 11:50:02 +0900

    タブメニュー実装、レイアウト要調整

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+{
+  const tabItems = document.querySelectorAll('.toiletTab li a');
+  const contents = document.querySelectorAll('.content');
+
+  tabItems.forEach(clickedItem => {
+    clickedItem.addEventListener('click', e => {
+      e.preventDefault();
+
+      tabItems.forEach(item => {
+        item.classList.remove('active');
+      });
+      clickedItem.classList.add('active');
+
+      contents.forEach(content => {
+        content.classList.remove('active');
+      });
+      document.getElementById(clickedItem.dataset.id).classList.add('active');
+    });
+  });
+}
